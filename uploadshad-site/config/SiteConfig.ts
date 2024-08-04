282a43a6f8207c5d8f1336a9b0e4840566201648
MeatBoyed <charlierossouw@outlook.com>
2024-08-04 02:26:29 +0200

    feat: Readme updated, and siteconfig url updated

--- uploadshad-site/config/SiteConfig.ts
+++ uploadshad-site/config/SiteConfig.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   author: "Charles Rossouw",
   description: `A highly customizable, accessible, and unopinionated Image Uploader component built with Shadcn UI.`,
   ogImage: "https://emblor.jaleelbennett.com/og.jpg",
-  url: "",
+  url: "https://uploadshad.nerfdesigns.com",
   links: {
     twitter: "https://twitter.com/meatboyed",
     instagram: "https://www.instagram.com/charlesrossouww/",
