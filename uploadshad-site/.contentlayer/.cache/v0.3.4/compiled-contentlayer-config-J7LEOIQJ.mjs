611ef28f3daf17fb1cc58794807d1cde805f485d
MeatBoyed <charlierossouw@outlook.com>
2024-08-04 21:52:10 +0200

    feat: Branding added to footer

---
+++ uploadshad-site/.contentlayer/.cache/v0.3.4/compiled-contentlayer-config-J7LEOIQJ.mjs
@@ -0,0 +1,53 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypePrettyCode from "rehype-pretty-code";
+var Docs = defineDocumentType(() => ({
+  name: "Docs",
+  filePathPattern: `docs/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string",
+      required: true
+    }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => `/${doc._raw.flattenedPath}`
+    },
+    slugAsParams: {
+      type: "string",
+      resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: [Docs],
+  disableImportAliasWarning: true,
+  mdx: {
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        // temprorary fix for typescript error. fix later
+        {
+          keepBackground: false,
+          theme: {
+            dark: "github-dark",
+            light: "github-light"
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Docs,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-J7LEOIQJ.mjs.map
--- uploadshad-site/components/Footer.tsx
+++ uploadshad-site/components/Footer.tsx
@@ -2,8 +2,8 @@ import { siteConfig } from "@/config/SiteConfig";
 
 export function SiteFooter() {
   return (
-    <footer className="py-6 md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+    <footer className="py-6 flex flex-col justify-center items-center gap-4 mb-8 md:px-8 ">
+      <div className="min-h-fit flex flex-col items-center justify-between  gap-4 md:h-24 w-full">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
           Built by{" "}
           <a
@@ -20,6 +20,16 @@ export function SiteFooter() {
           </a>
           .
         </p>
+        <div className="w-full border" />
+        <div className="w-full  min-h-fit flex justify-center items-center flex-col gap-5">
+          <div className="flex justify-center items-center flex-col">
+            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Powered by</h3>
+            <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight text-[#00AAFF] ">Nerf Designs</h2>
+            <div className="leading-7">Nerf your competition</div>
+          </div>
+
+          <p className="leading-7 text-center">Copyright © {new Date().getFullYear()} Nerf Designs. All Rights Reserved</p>
+        </div>
       </div>
     </footer>
   );
